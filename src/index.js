71bc79369c4e77bc6e177ec3b5881fd829525b06
krisbobo <kiyongbohbo@gmail.com>
2023-04-18 02:55:43 +0100

    Add routes to various pages and render app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Rocket from './pages/Rocket';
+import Mission from './pages/Mission';
+import Profile from './pages/Profile';
+import Error from './components/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Rocket /> },
+      { path: '/missions', element: <Mission /> },
+      { path: '/profile', element: <Profile /> },
+      { path: '*', element: <Error /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
