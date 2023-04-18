c9b56c758f9fa7901a540cf37ac4b08132dbaaa2
krisbobo <kiyongbohbo@gmail.com>
2023-04-18 15:53:03 +0100

    Disable linter for unused var

--- src/redux/missions/missionSlice.js
+++ src/redux/missions/missionSlice.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable no-unused-vars */
 const url = 'https://api.spacexdata.com/v3/missions';
 
 const initialState = {
--- src/redux/rockets/rocketSlice.js
+++ src/redux/rockets/rocketSlice.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable no-unused-vars */
 const url = 'https://api.spacexdata.com/v4/rockets';
 
 const initialState = {
