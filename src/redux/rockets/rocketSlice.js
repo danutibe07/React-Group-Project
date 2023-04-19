d9ac4f445fdd03d484484c91a6e30c6a0325051b
krisbobo <kiyongbohbo@gmail.com>
2023-04-19 19:32:55 +0100

    Fetch rockets

--- src/redux/rockets/rocketSlice.js
+++ src/redux/rockets/rocketSlice.js
@@ -1,7 +1,43 @@
-/* eslint-disable no-unused-vars */
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit'
+
+const GET_ROCKETS = 'Rockets/Rockets/GET_ROCKETS';
 const url = 'https://api.spacexdata.com/v4/rockets';
 
+const getData = async () => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
 const initialState = {
-  missions: [],
+  rockets: [],
   isLoading: false,
 };
+
+const rocketReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case `${GET_ROCKETS}\fulfilled`:
+      return {
+        ...state,
+        rockets: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const getRockets = createAsyncThunk(GET_ROCKETS, async () => {
+  const data = await getData();
+  const rockets = [];
+
+  data.forEach((data) => {
+    data.push(data);
+  });
+  return rockets;
+});
+
+export default rocketReducer;
