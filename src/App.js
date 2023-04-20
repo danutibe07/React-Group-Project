7c6169d1beac0e0a7f6d5099bdce4d63a95f49fc
krisbobo <kiyongbohbo@gmail.com>
2023-04-20 23:27:03 +0100

    Use BrowserRouter and routes for navigation
    Previous routing techniques prevented the display of the rendered page on the browser. However, even using this routing technique didn't resolve the issue but I am sticking to it due to its relatively simplicity

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import { Outlet } from 'react-router-dom';
+import {
+  BrowserRouter, Route, Routes, Navigate,
+} from 'react-router-dom';
 import Header from './components/Header';
+import Rocket from './pages/Rocket';
+import Mission from './pages/Mission';
+import Profile from './pages/Profile';
+import Error from './components/Error';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <div className="nav-bar">
         <Header />
       </div>
-      <main className="container">
-        <Outlet />
-      </main>
-    </>
+      <div className="container">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Navigate to="/rockets" replace />}
+          />
+          <Route
+            path="/rockets"
+            element={<Rocket />}
+            exact
+          />
+          <Route
+            path="/missions"
+            element={<Mission />}
+            exact
+          />
+          <Route
+            path="/profile"
+            element={<Profile />}
+            exact
+          />
+          <Route
+            path="*"
+            element={<Error />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
