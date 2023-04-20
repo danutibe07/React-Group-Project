476f176a7b9d2a7a8591eb8dff53f062d9843406
krisbobo <kiyongbohbo@gmail.com>
2023-04-20 23:22:05 +0100

    Dispatch fetched data and render

--- src/pages/Rocket.js
+++ src/pages/Rocket.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRockets } from '../redux/rockets/rocketSlice';
+import style from '../components/Rocket.module.css';
 
-const Rocket = () => (
-  <div>Rocket</div>
-);
+const Rocket = () => {
+  const { rockets } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
+
+  return (
+    rockets.map((rocket) => (
+      <section key={rocket.id} className={style.rocketContainer}>
+        <div className={style.image}>
+          <img src={rocket.flickr_images[0]} alt="rocket" />
+        </div>
+        <div className={style.content}>
+          {' '}
+          <h2 className={style.rocketName}>{rocket.rocket_name}</h2>
+          <p className={style.rocketDescription}>{rocket.description}</p>
+          <button type="button" className={style.reserve}>Reserve Rocket</button>
+        </div>
+      </section>
+    ))
+  );
+};
 
 export default Rocket;
