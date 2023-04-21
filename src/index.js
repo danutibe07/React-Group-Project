9359bb91ee7e407f2eb73b02fe096db97723dc60
danutibe07 <danielutibe07@gmail.com>
2023-04-21 12:33:55 +0100

    added page routing

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
-import { Provider } from 'react-redux';
-import Rocket from './pages/Rocket';
-import Mission from './pages/Mission';
-import Profile from './pages/Profile';
-import Error from './components/Error';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      { path: '/', element: <Rocket /> },
-      { path: '/missions', element: <Mission /> },
-      { path: '/profile', element: <Profile /> },
-      { path: '*', element: <Error /> },
-    ],
-  },
-]);
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
