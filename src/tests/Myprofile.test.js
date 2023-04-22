9f38f0ac62b1d7174131d4cfa4e0ce24bf125e7d
danutibe07 <danielutibe07@gmail.com>
2023-04-22 01:36:29 +0100

    added test

---
+++ src/tests/Myprofile.test.js
@@ -0,0 +1,18 @@
+import Myprofile from '../pages/Myprofile';
+import ProfileMission from '../pages/Myprofile';
+
+describe('Testing the profile', () => {
+  const component = (
+    <Myprofile mission={{
+      mission_id: '1',
+      mission_name: 'Test ',
+      description: 'Test  description',
+      reserved: true,
+    }}
+    />
+  );
+
+  test('should match snapshot', () => {
+    expect(component).toMatchSnapshot();
+  });
+});
